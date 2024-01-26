0c5523b0e3d86172e8a9be239aefea9561692b4c
carleb <calebcheongdnt@gmail.com>
2024-01-26 14:58:47 +0800

    hekp

--- app/store/urls.py
+++ app/store/urls.py
@@ -1,21 +1,14 @@
-
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-
-
     # Store main page
-
-    path('', views.store, name='store'),
-
-
+    path("", views.store, name="store"),
     # Individual product
-
-    path('product/<slug:product_slug>/', views.product_info, name='product-info'),
-
-    path('store/upload_product' ,views.upload_product, name='upload-product'),
-
-
+    path("product/<slug:product_slug>/", views.product_info, name="product-info"),
+    path("store/upload_product", views.upload_product, name="upload-product"),
+    path(
+        "store/edit_product/<int:listing_id>/", views.edit_product, name="edit-product"
+    ),
 ]
--- app/store/views.py
+++ app/store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from . models import Category, Product
+from .models import Category, Product
 
 from django.shortcuts import get_object_or_404
 
@@ -12,48 +12,45 @@ from PIL import Image
 
 
 def store(request):
-
     # all_products = Product.objects.all()
     all_products = Product.objects.filter(quantity__gt=0)
 
-    context = {'my_products':all_products}
+    context = {"my_products": all_products}
 
-    return render(request, 'store/store.html', context)
-
+    return render(request, "store/store.html", context)
 
 
 def categories(request):
-
     all_categories = Category.objects.all()
 
-    return {'all_categories': all_categories}
-
+    return {"all_categories": all_categories}
 
 
 def list_category(request, category_slug=None):
-
     category = get_object_or_404(Category, slug=category_slug)
 
     products = Product.objects.filter(category=category)
 
-
-    return render(request, 'store/list-category.html', {'category':category, 'products':products})
-
+    return render(
+        request,
+        "store/list-category.html",
+        {"category": category, "products": products},
+    )
 
 
 def product_info(request, product_slug):
-
     product = get_object_or_404(Product, slug=product_slug)
 
-    context = {'product': product}
+    context = {"product": product}
 
-    return render(request, 'store/product-info.html', context)
+    return render(request, "store/product-info.html", context)
 
 
 @login_required
 def upload_product(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
+<<<<<<< Updated upstream
         if form.is_valid():
             product = form.save(commit=False)
             product.seller = request.user.email
@@ -61,13 +58,34 @@ def upload_product(request):
             form.save()
 
             return redirect('store')
+=======
+        print("PRINTING HERE")
+        print(form)
+        print("End of PRINTING HERE")
+
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.seller = request.user.email
+
+            form.save()
+
+            return redirect("store")
+>>>>>>> Stashed changes
     else:
         form = ProductForm()
-    return render(request, 'store/upload_product.html', {'form': form})
+    return render(request, "store/upload_product.html", {"form": form})
 
 
+@login_required
+def edit_product(request, listing_id):
+    listing = get_object_or_404(Product, id=listing_id)
 
-
-
-
-
+    # Check if the current user is the seller of the product
+    if listing.seller.email != request.user.email:
+        print("NOT OG USER")  # It's better to use logging instead of print
+        return redirect(
+            "name_of_your_store_view"
+        )  # Replace with your store's view name
+    else:
+        context = {"my_products": listing}
+        return render(request, "store/edit_product.html", context)
