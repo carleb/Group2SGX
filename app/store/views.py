19ed44814705a152ca9aaab2add8c29437d9abc9
carleb <calebcheongdnt@gmail.com>
2024-01-26 15:44:22 +0800

    edit

--- app/store/views.py
+++ app/store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from . models import Category, Product
+from .models import Category, Product
 
 from django.shortcuts import get_object_or_404
 
@@ -12,47 +12,43 @@ from PIL import Image
 
 
 def store(request):
-
     # all_products = Product.objects.all()
     all_products = Product.objects.filter(quantity__gt=0)
 
-    context = {'my_products':all_products}
+    context = {"my_products": all_products}
 
-    return render(request, 'store/store.html', context)
-
+    return render(request, "store/store.html", context)
 
 
 def categories(request):
-
     all_categories = Category.objects.all()
 
-    return {'all_categories': all_categories}
-
+    return {"all_categories": all_categories}
 
 
 def list_category(request, category_slug=None):
-
     category = get_object_or_404(Category, slug=category_slug)
 
     products = Product.objects.filter(category=category)
 
-
-    return render(request, 'store/list-category.html', {'category':category, 'products':products})
-
+    return render(
+        request,
+        "store/list-category.html",
+        {"category": category, "products": products},
+    )
 
 
 def product_info(request, product_slug):
-
     product = get_object_or_404(Product, slug=product_slug)
 
-    context = {'product': product}
+    context = {"product": product}
 
-    return render(request, 'store/product-info.html', context)
+    return render(request, "store/product-info.html", context)
 
 
 @login_required
 def upload_product(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             product = form.save(commit=False)
@@ -60,14 +56,33 @@ def upload_product(request):
 
             form.save()
 
-            return redirect('store')
+            return redirect("store")
     else:
         form = ProductForm()
-    return render(request, 'store/upload_product.html', {'form': form})
+    return render(request, "store/upload_product.html", {"form": form})
 
 
+@login_required
+def edit_product(request, listing_id):
+    listing = get_object_or_404(Product, id=listing_id)
+    print(listing)
+    if request.method == "POST":
+        # The form is submitted with data for updating the product
+        form = ProductForm(request.POST, request.FILES, instance=listing)
+        print(form)
+        print(form.is_valid())
+        if form.is_valid():
+            form.save()
+            product = form.save(commit=False)
+            product.seller = request.user.email
+            return redirect("store")
+        else:
+            # Print the form errors in the console
+            print(form.errors)
 
 
+    form = ProductForm(instance=listing)
 
-
-
+    return render(
+        request, "store/edit_product.html", {"listing": listing, "form": form}
+    )
